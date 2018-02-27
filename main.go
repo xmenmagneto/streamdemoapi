3307fce5049b473f5a7bfcee77dc492ffb2a6b60
Tingchang Deng <tingchangdeng@Tingchangs-MacBook-Pro.local>
2018-02-27 14:57:04 -0800

    add stream and messages

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+/*
+ccm create -v 2.1.1 streamdemoapi
+ccm populate -n 1
+echo "CREATE KEYSPACE demoapi WITH replication = {'class': 'SimpleStrategy', 'replication_factor' : 1};" | cqlsh
+echo "use streamdemoapi; drop table messages; create table messages (id UUID, user_id UUID, message text, PRIMARY KEY(id));" | cqlsh
+echo "use streamdemoapi; create index on messages (user_id);" | cqlsh
+echo "use streamdemoapi; drop table users; CREATE TABLE users ( id UUID, firstname text, lastname text, age int, email text, city text, PRIMARY KEY (id));" | cqlsh
+*/
+
 import (
 	"net/http"
 	"log"
@@ -9,23 +18,36 @@ import (
 	"github.com/xmenmagneto/streamdemoapi/Users"
 )
 
-type heartbeatResponse struct {
-	Status string `json:"status"`
-	Code int `json:"code"`
-}
-
 func main() {
- 	CassandraSession := Cassandra.Session
-  	defer CassandraSession.Close()
+	err := Stream.Connect(
+		"ax3bm9tjcb35",
+		"ec9tydddc78zmmc8r682j43z5y6exkaa98myamzk23pztm5yb8s4c66g5737eyey",
+		"us-east")
+	if err != nil {
+		log.Fatal("Could not connect to Stream, abort")
+	}
+
+	CassandraSession := Cassandra.Session
+	defer CassandraSession.Close()
 
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", heartbeat)
 
-	router.HandleFunc("/users/new", Users.Post)
 	router.HandleFunc("/users", Users.Get)
+	router.HandleFunc("/users/new", Users.Post)
 	router.HandleFunc("/users/{user_uuid}", Users.GetOne)
 
+	router.HandleFunc("/messages", Messages.Get)
+	router.HandleFunc("/messages/new", Messages.Post)
+	router.HandleFunc("/messages/{message_uuid}", Messages.GetOne)
+
 	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+
+type heartbeatResponse struct {
+	Status string `json:"status"`
+	Code   int `json:"code"`
 }
 
 func heartbeat(w http.ResponseWriter, r *http.Request) {
